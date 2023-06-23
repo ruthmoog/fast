bb18e535fc5923876ac48be005a8db2501bd4006
Ruth Baker <ruthmoog@gmail.com>
2023-06-23 15:04:14 +0100

    Score two ends in a table

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -39,16 +39,61 @@ function ScoreButton({setScores, value}) {
 }
 
 function ScoreSheet({scores}) {
+
+    const perChunk = 6 // items per chunk
+
+    const ends = scores.reduce((resultArray, item, index) => {
+        const chunkIndex = Math.floor(index / perChunk)
+
+        if (!resultArray[chunkIndex]) {
+            resultArray[chunkIndex] = [] // start a new chunk
+        }
+
+        resultArray[chunkIndex].push(item)
+
+        return resultArray
+    }, [])
+
+    console.log(ends);
+
+
     return (
         <>
-            <h1>Current scores</h1>
-            <ol>
-                {scores.map(score => (<li>{score}</li>))}
-            </ol>
+            <h1>Your Scores</h1>
+            <table style={{border: "1px solid black"}}>
+                <thead>
+                <tr style={{border: "1px solid black"}}>
+                    <th style={{border: "1px solid black"}} colSpan={6}>🎯 scores</th>
+                    <th style={{border: "1px solid black"}}>E/T</th>
+                    <th style={{border: "1px solid black"}} colSpan={6}>🎯 scores</th>
+                    <th style={{border: "1px solid black"}}>E/T</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr style={{border: "1px solid black"}}>
+                    {ends.map((endScores) => (<End endScores={endScores}/>))}
+                </tr>
+                </tbody>
+            </table>
         </>
     )
 }
 
+function End({endScores}) {
+    const endTotal = endScores.reduce((totalScore, score) => {
+        if (score === 'M') {
+            return totalScore
+        }
+
+        return totalScore + score
+    }, 0)
+    return (
+        <>
+            {endScores.map((score) => (<td style={{border: "1px solid black"}}>{score}</td>))}
+            <td style={{border: "1px solid black"}}>{endTotal}</td>
+        </>
+    );
+}
 
 function RunningTotal({scores}) {
     const runningTotal = scores.reduce((total, score) => {
