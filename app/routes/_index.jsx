5787b5bafa77057bc0224685ae1e163a0a48df71
Ruth Baker <ruthmoog@gmail.com>
2023-06-23 15:42:58 +0100

    Show a round with 2 ends of 6 scores each plus their subtotals

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -40,10 +40,10 @@ function ScoreButton({setScores, value}) {
 
 function ScoreSheet({scores}) {
 
-    const perChunk = 6 // items per chunk
+    const scoresPerEnd = 6 // items per chunk
 
     const ends = scores.reduce((resultArray, item, index) => {
-        const chunkIndex = Math.floor(index / perChunk)
+        const chunkIndex = Math.floor(index / scoresPerEnd)
 
         if (!resultArray[chunkIndex]) {
             resultArray[chunkIndex] = [] // start a new chunk
@@ -54,8 +54,19 @@ function ScoreSheet({scores}) {
         return resultArray
     }, [])
 
-    console.log(ends);
+    const endsPerRound = 2 // items per chunk
 
+    const rounds = ends.reduce((resultArray, item, index) => {
+        const chunkIndex = Math.floor(index / endsPerRound)
+
+        if (!resultArray[chunkIndex]) {
+            resultArray[chunkIndex] = [] // start a new chunk
+        }
+
+        resultArray[chunkIndex].push(item)
+
+        return resultArray
+    }, [])
 
     return (
         <>
@@ -67,12 +78,20 @@ function ScoreSheet({scores}) {
                     <th style={{border: "1px solid black"}}>E/T</th>
                     <th style={{border: "1px solid black"}} colSpan={6}>🎯 scores</th>
                     <th style={{border: "1px solid black"}}>E/T</th>
+                    <th style={{border: "1px solid black"}}>H</th>
+                    <th style={{border: "1px solid black"}}>S</th>
+                    <th style={{border: "1px solid black"}}>G</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr style={{border: "1px solid black"}}>
-                    {ends.map((endScores) => (<End endScores={endScores}/>))}
-                </tr>
+                {rounds.map((e) => (
+                    <tr style={{border: "1px solid black"}}>
+                        <End endScores={e[0] ?? []} />
+                        <End endScores={e[1] ?? []} />
+                        <RoundSubTotals ends={e}/>
+                    </tr>
+                ))}
+
                 </tbody>
             </table>
         </>
@@ -89,10 +108,51 @@ function End({endScores}) {
     }, 0)
     return (
         <>
-            {endScores.map((score) => (<td style={{border: "1px solid black"}}>{score}</td>))}
+            <td style={{border: "1px solid black"}}>{endScores[0] ?? ''}</td>
+            <td style={{border: "1px solid black"}}>{endScores[1] ?? ''}</td>
+            <td style={{border: "1px solid black"}}>{endScores[2] ?? ''}</td>
+            <td style={{border: "1px solid black"}}>{endScores[3] ?? ''}</td>
+            <td style={{border: "1px solid black"}}>{endScores[4] ?? ''}</td>
+            <td style={{border: "1px solid black"}}>{endScores[5] ?? ''}</td>
+
             <td style={{border: "1px solid black"}}>{endTotal}</td>
         </>
     );
+}
+
+function RoundSubTotals({ends}) {
+    console.log("Ends: ", ends)
+    const scores = ends.flat()
+
+    const hitsCount = scores.reduce((total, score) => {
+        if (score !== 'M') {
+            return total + 1
+        }
+        return total
+    }, 0)
+
+    const roundScore = scores.reduce((total, score) => {
+        if (score === 'M') {
+            return total
+        }
+
+        return total + score
+    }, 0)
+
+    const goldCount = scores.reduce((total, score) => {
+        if (score === 9) {
+            return total + 1
+        }
+        return total
+    }, 0)
+
+    return (
+        <>
+            <td>{hitsCount}</td>
+            <td>{roundScore}</td>
+            <td>{goldCount}</td>
+        </>
+    )
 }
 
 function RunningTotal({scores}) {
