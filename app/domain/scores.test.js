b41e0a387a9c0b1e460386e0783977a80c24bb77
Ruth Baker <ruthmoog@gmail.com>
2023-06-27 09:42:31 +0100

    Unit tests calculateGoldCount

--- app/domain/scores.test.js
+++ app/domain/scores.test.js
@@ -1,4 +1,4 @@
-import {calculateHitsCount, calculateTotal, validScores} from "./scores";
+import {calculateGoldCount, calculateHitsCount, calculateTotal, validScores} from "./scores";
 
 // note to ruth: https://vitest.dev/
 test("it knows the valid scores of imperial archery", () => {
@@ -17,13 +17,25 @@ test.each([
 });
 
 test.each([
-  [[1, 3, 5, 7, 9, 7], 6],
-  [[1, 3, 5, 7, 9, "M"], 5],
-  [[1, 3, 5, 7, "M", "M"], 4],
-  [[1, 3, 5, "M", "M", "M"], 3],
-  [[1, 3, "M", "M", "M", "M"], 2],
-  [[1, "M", "M", "M", "M", "M"], 1],
+  [[1,   3,   5,   7,   9,   7  ], 6],
+  [[1,   3,   5,   7,   9,   "M"], 5],
+  [[1,   3,   5,   7,   "M", "M"], 4],
+  [[1,   3,   5,   "M", "M", "M"], 3],
+  [[1,   3,   "M", "M", "M", "M"], 2],
+  [[1,   "M", "M", "M", "M", "M"], 1],
   [["M", "M", "M", "M", "M", "M"], 0]
 ])("it can calculate hits", (scores, expectedHitCount) => {
   expect(calculateHitsCount(scores)).toEqual(expectedHitCount);
+});
+
+test.each([
+  [[9, 9, 9, 9,   9, 9], 6],
+  [[1, 9, 9, 9,   9, 9], 5],
+  [[1, 3, 9, 9,   9, 9], 4],
+  [[1, 3, 5, 9,   9, 9], 3],
+  [[1, 3, 5, "M", 9, 9], 2],
+  [[1, 3, 5, "M", 1, 9], 1],
+  [[1, 3, 5, "M", 1, 7], 0]
+])("it can calculate golds", (scores, expectedGoldsCount) => {
+  expect(calculateGoldCount(scores)).toEqual(expectedGoldsCount);
 });
