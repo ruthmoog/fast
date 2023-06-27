572d35122b6f0d9d974d07692012eed9a636f190
Ruth Baker <ruthmoog@gmail.com>
2023-06-27 09:49:49 +0100

    Refactor calculateTotals

--- app/domain/scores.js
+++ app/domain/scores.js
@@ -3,14 +3,9 @@ export const validScores = [1, 3, 5, 7, 9, MISS];
 export const scoresPerEnd = 6;
 export const endsPerRound = 2;
 
-
 export function calculateTotal(scores) {
-  return scores.reduce((totalScore, score) => {
-    if (score === MISS) {
-      return totalScore;
-    }
-    return totalScore + score;
-  }, 0);
+  const hits = scores.filter((score) => score !== MISS)
+  return hits.reduce((totalScore, score) => totalScore + score)
 }
 
 export function calculateHitsCount(scores) {
