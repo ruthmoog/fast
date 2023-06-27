4006782bf66a047d0c85bd540da0af4b3bc77cc6
Chris James <quii@hey.com>
2023-06-27 08:36:11 +0100

    quick table test to exercise calculating totals

--- app/domain/scores.test.js
+++ app/domain/scores.test.js
@@ -1,5 +1,17 @@
-import { validScores } from "./scores";
+import { calculateTotal, validScores } from "./scores";
 
-test('it knows the valid scores of imperial archery', () => {
-    expect(validScores).toEqual([1, 3, 5, 7, 9, 'M'])
-} )
+// note to ruth: https://vitest.dev/
+test("it knows the valid scores of imperial archery", () => {
+  expect(validScores).toEqual([1, 3, 5, 7, 9, "M"]);
+});
+
+test.each([
+  [[1, 3, 5, 7, 9, "M"], 25],
+  [[1, 3, 5, 7, 9, 1], 26],
+  [[1, 3, 5, 7, 9, 3], 28],
+  [[1, 3, 5, 7, 9, 5], 30],
+  [[1, 3, 5, 7, 9, 7], 32],
+  [[1, 3, 5, 7, 9, 9], 34]
+])("it can calculate totals", (scores, expectedTotal) => {
+  expect(calculateTotal(scores)).toEqual(expectedTotal);
+});
